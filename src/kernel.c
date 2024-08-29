5a8d285cfad70ac3b5d5eae59f1991876c42f563
BrittonPearce <bp0082@uah.edu>
2024-08-29 18:51:04 -0500

    Add basic terminal scrolling

--- src/kernel.c
+++ src/kernel.c
@@ -7,4 +7,16 @@ void kernel_main(void) {
   term_writeline("Testing writeline");
   term_err("This is an error\n");
   term_warn("This is a warning\n");
+  
+  // TODO (Britton): Remove this test code for proving that the terminal code scrolls
+  uint32_t x = 0;
+  while(1) {
+    char s[3] = {0, '\n', '\0'};
+    *s = x % 10 + '0';
+    x++;
+    term_write(s);
+    for(unsigned int i = 0; i < 0x8fff000; i++) {
+    }
+  }
+
 }
