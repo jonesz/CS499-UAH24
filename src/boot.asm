1a4cc0d4395960fc281c5ec3b99943a9bc2a4c12
BrittonPearce <bp0082@uah.edu>
2024-09-03 18:39:22 -0500

    DEBUG gdt

--- src/boot.asm
+++ src/boot.asm
@@ -20,8 +20,17 @@ stack_bottom:
         resb 16384 ; 16 KiB reserved.
 stack_top:
 
+; Prepare space for the GDT
+section .data
+align 32
+gdt_base times 48 db 0 ; 8 byte long GDT entries * 6 required entries = 48B reserved
+align 16
+gdt_descriptor  DW 0
+                DD 0
+
 ; Bootloader jumps to this `_start` as specified by the linker.
 section .text
+
 global _start:function (_start.end - _start)
 _start:
 
@@ -29,10 +38,32 @@ _start:
 ; paging is disabled. Setup the stack.
         mov esp, stack_top
 
-; TODO: Setup GDT/Paging here and any state that is needed before
+; TODO: Setup Paging here and any state that is needed before
 ; the kernel is loaded.
+        ; Sets up gdt
+        mov eax, gdt_descriptor
+        mov ebx, gdt_base
+        mov word [gdt_descriptor], 47
+        mov [gdt_descriptor + 2], ebx 
+        push ebx
+        extern set_up_gdt
+        call set_up_gdt
+        lgdt [gdt_descriptor]
+        ; TODO(Britton): Trying to reload cs fails on my machine,
+        ; but in testing it was already set to 8.
+        ; Figure out why it is not working to be sure we manually set it.
+        ; jmp 0x8:reload_cs
+        .reload_cs:
+        mov ax, 0x10
+        mov ds, ax
+        mov es, ax
+        mov fs, ax
+        mov gs, ax
+        mov ss, ax
 
 ; Enter the main kernel.
+        mov ebx, gdt_base
+        push ebx
         extern kernel_main
         call kernel_main
 
