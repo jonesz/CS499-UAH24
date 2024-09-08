1919822606b8e21d0e81f30e3b179b22274d96cd
Ethan Jones <etn.jones@gmail.com>
2024-09-07 21:51:08 -0500

    ported PDCLIB.
    There's a Makefile.PDCLIB which is copied over to the pdclib directory,
    we build a static libk.a, then link it in here. It's not great, but
    it works.

--- src/build_libc.sh
+++ src/build_libc.sh
@@ -1,11 +1,10 @@
 SRC=$PWD
 
+cp Makefile.PDCLIB ../ext/pdclib/Makefile
 cp -r $SRC/uah-os-pdclib/* ../ext/pdclib
 cd ../ext/pdclib
-_PDCLIB_BUILD=1 _PDCLIB_STATIC_DEFINE=1 __STDC_NO_THREADS__=1 cmake .
-make -j4
+make
+cp libk.a $SRC/
 
 mkdir -p $SRC/include
 cp -r include/* $SRC/include
-
-cp libpdclib.a $SRC
