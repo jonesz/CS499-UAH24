f907aba1c68c27383dd8c6935a83663133a71593
BrittonPearce <bp0082@uah.edu>
2024-09-14 13:00:39 -0500

    Force casting to poitners later

--- src/multiboot.h
+++ src/multiboot.h
@@ -11,19 +11,19 @@ struct _BootInformation {
   uint32_t cmd_line;
 
   uint32_t mods_count;
-  uint32_t *mods_addr;
+  uint32_t mods_addr;
 
   uint32_t syms[4];
 
   uint32_t mmap_length;
-  uint32_t *mmap_addr;
+  uint32_t mmap_addr;
 
   uint32_t drives_length;
-  uint32_t *drives_addr;
+  uint32_t drives_addr;
 
-  uint32_t *config_table;
+  uint32_t config_table;
   uint32_t boot_loader_name;
-  uint32_t *apm_table;
+  uint32_t apm_table;
 
   uint32_t vbe_control_info;
   uint32_t vbe_mode_info;
