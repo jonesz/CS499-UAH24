0f1f1503f07f85967cb701257d9fa69d3673119c
BrittonPearce <bp0082@uah.edu>
2024-09-14 13:30:57 -0500

    Get BootInfo structure from grub

--- src/kernel.c
+++ src/kernel.c
@@ -14,20 +14,7 @@ void kernel_main(BootInformation *boot_info) {
   void *loc = kernel_main;
   term_format("This is a format string hex: %x\n", &tmp);
   term_format("This is a format string hex: %x\n", &loc);
-
-  for (int i = 31; i >= 0; i--) {
-    uint32_t x = boot_info->flags & ((uint32_t)1 << i);
-    char c[2] = {'0', 0};
-    c[0] += !!x;
-    term_write(c);
-  }
-  term_write("\n");
-  for (int i = 31; i >= 0; i--) {
-    uint32_t x = boot_info->cmd_line & ((uint32_t)1 << i);
-    char c[2] = {'0', 0};
-    c[0] += !!x;
-    term_write(c);
-  }
-  term_write("\n");
   term_write((const char *)boot_info->cmd_line);
+  term_write("\n");
+
 }
