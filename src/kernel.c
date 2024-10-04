d6a145e271dbd3491182aa5e86da1b05ee1e6eda
BrittonPearce <bp0082@uah.edu>
2024-10-04 18:44:26 -0500

    ACPI to check for Serial Controller

--- src/kernel.c
+++ src/kernel.c
@@ -1,9 +1,11 @@
 /** src/kernel.c */
+#include "acpi.h"
 #include "kmalloc.h"
 #include "multiboot.h"
 #include "term.h"
 #include "test/test.h"
 #include <stddef.h>
+#include <string.h>
 
 extern multiboot_info_t *boot_info;
 
@@ -24,7 +26,65 @@ void kernel_main() {
 
 #ifndef RELEASE
   test_all_functions();
+  term_write("\n");
+
 #endif
-  
-  __asm__("int $0");
+
+  // This ridiculous code finds the RSDP, which is used to find the ACPI
+  // The ACPI holds important system information
+  uint8_t sum = 0;
+  uint32_t sum32 = sum;
+  RSDP_t *rsdp = 0;
+  char *rsd_ptr_str = "RSD PTR ";
+  for (uint8_t *i = (uint8_t *)0x000E0000; i < (uint8_t *)0x000FFFFF; i++) {
+    if (strncmp(rsd_ptr_str, (char *)i, 8) == 0) {
+      rsdp = (RSDP_t *)i;
+    }
+  }
+  // Checksum to make sure RSDP is valid
+  for (size_t i = 0; i < sizeof(*rsdp); i++) {
+    sum += *(uint8_t *)((uint8_t *)rsdp + i);
+  }
+  sum32 = sum;
+  if (sum != 0) {
+    term_format("Error: RSDP Checksum Failed - %x\n", &sum32);
+    return;
+  }
+  sum = 0;
+
+  // After verifying the RSDP, move on to the actual Root System Description
+  RSDT *rsdt = (RSDT *)rsdp->RsdtAddress;
+  for (size_t i = 0; i < rsdt->h.Length; i++) {
+    sum += *(uint8_t *)((uint8_t *)rsdt + i);
+  }
+  sum32 = sum;
+  if (sum != 0) {
+    term_format("Error: RSDT Checksum Failed - %x\n", &sum32);
+    return;
+  }
+  sum = 0;
+
+  // After verifying the RSDT, move on to the actual FADT to check for a serial controller
+  FADT *fadt = (FADT *)rsdt->PointerToOtherSDT;
+  // Signature 'FADT' (non-null-termianted, ignore trailing characters) shows if we are
+  // At the correct table
+  term_format("%s\n", fadt->h.Signature);
+  for (size_t i = 0; i < fadt->h.Length; i++) {
+    sum += *(uint8_t *)((uint8_t *)fadt + i);
+  }
+  sum32 = sum;
+  if (sum != 0) {
+    term_format("Error: FADT Checksum Failed - %x\n", &sum32);
+    return;
+  }
+
+  /* NOTE(BP): BootArchitectureFlags should tell us if a serial controller exists
+  As a double check, The value should be at offset 109 according to this page
+  https://wiki.osdev.org/%228042%22_PS/2_Controller#Step_2:_Determine_if_the_PS/2_Controller_Exists  // 
+  These are off by what appears to be one byte,
+  but the relevant flag is zero either way on my machine.
+  So, it seems that there is no serial controller for I/O
+  */
+  term_format("BAF: %x\n", &(fadt->BootArchitectureFlags));
+  term_format("BAF (DOUBLE CHECK): %x\n", (uint8_t *)(fadt) + 109);
 }
