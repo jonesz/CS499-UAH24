262aa00119d0d5831c1077e8492966204949395d
Ethan Jones <etn.jones@gmail.com>
2024-11-27 23:24:48 -0600

    add a spawn call.

--- src/include/syscalls/syscalls.h
+++ src/include/syscalls/syscalls.h
@@ -13,7 +13,7 @@ typedef struct _syscall_info_t syscall_info_t;
 
 typedef struct _msg_t msg_t;
 
-enum _syscall_id_t {Sys_Send, Sys_Recv, Sys_Sleep, Sys_Exit, Sys_Write};
+enum _syscall_id_t {Sys_Send, Sys_Recv, Sys_Sleep, Sys_Exit, Sys_Spawn};
 
 struct  _syscall_info_t {
     void* args;
@@ -30,6 +30,6 @@ uint32_t send(msg_t* msg, uint32_t comm_channel);
 uint32_t recv(msg_t* msg_dest, uint32_t comm_channel);
 uint32_t sleep(uint32_t ticks);
 uint32_t exit();
-uint32_t write(msg_t* msg);
+uint32_t spawn(msg_t* msg);
 
 #endif
