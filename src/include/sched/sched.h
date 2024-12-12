0016dd0352b4a16042045f17caf17a8919a9a0eb
ethan jones <erj0005@uah.edu>
2024-12-12 10:45:07 -0600

    Sched changes, we need to return some values...

--- src/include/sched/sched.h
+++ src/include/sched/sched.h
@@ -10,14 +10,17 @@
 typedef struct _scheduler_t {
   // A set of running processes.
   pcb_t process_table[MAX_PROCESSES];
+  // The process that is blocking the specific index.
+  uint32_t block_owner[MAX_PROCESSES];
 } scheduler_t;
 
 int sched_init();
-void sched_kill(uint32_t stack_loc);
+uint32_t sched_kill(uint32_t stack_loc);
 void sched_admit(uint32_t eip);
-void sched_admit_args(uint32_t eip, uint32_t argc, char** argv);
+uint32_t sched_admit_args(uint32_t eip, uint32_t argc, char** argv);
 void sched_interrupt(uint32_t counter, uint32_t stack_loc);
-void sched_block(uint32_t stack_loc);
-void sched_unblock();
+void sched_block(uint32_t stack_loc, uint32_t blocker);
+void sched_unblock(uint32_t blocker);
+uint32_t sched_who_is_running();
 
 #endif // SCHED_H
--- src/include/syscalls/syscalls.h
+++ src/include/syscalls/syscalls.h
@@ -13,7 +13,7 @@ typedef struct _syscall_info_t syscall_info_t;
 
 typedef struct _msg_t msg_t;
 
-enum _syscall_id_t {Sys_Send, Sys_Recv, Sys_Sleep, Sys_Exit, Sys_Spawn, Sys_Spawn_Bg};
+enum _syscall_id_t {Sys_Send, Sys_Recv, Sys_Sleep, Sys_Exit, Sys_Spawn, Sys_Spawn_Bg, Sys_PID};
 
 struct  _syscall_info_t {
     void* args;
@@ -23,6 +23,7 @@ struct  _syscall_info_t {
 struct  _msg_t {
     void* data;
     uint32_t length;
+    uint32_t sender;
 };
 
 
@@ -32,5 +33,6 @@ uint32_t sleep(uint32_t ticks);
 uint32_t exit();
 uint32_t spawn(uint32_t eip, uint32_t argc, char** argv);
 uint32_t spawn_bg(uint32_t eip, uint32_t argc, char** argv);
+uint32_t pid();
 
 #endif
