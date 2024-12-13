9133bf84f7fc6d2e20d5c413105fab9c0b4e6d09
Ethan Jones <etn.jones@gmail.com>
2024-12-13 06:35:07 -0600

    Updates to both syscalls and the program.

--- src/include/syscalls/syscalls.h
+++ src/include/syscalls/syscalls.h
@@ -1,8 +1,9 @@
 #ifndef SYSCALLS_H
 #define SYSCALLS_H
-
 #include <stdint.h>
 #include "sched/sched.h"
+
+/* For information about this file, see the `MiniOS Programmer's Manual.` */
 
 #define MSG_T_MAX 1024
 #define STDOUT (MAX_PROCESSES + 1)
@@ -10,7 +11,6 @@
 
 typedef enum _syscall_id_t syscall_id_t;
 typedef struct _syscall_info_t syscall_info_t;
-
 typedef struct _msg_t msg_t;
 
 enum _syscall_id_t {Sys_Send, Sys_Recv, Sys_Sleep, Sys_Exit, Sys_Spawn, Sys_Spawn_Bg, Sys_PID};
@@ -26,10 +26,10 @@ struct  _msg_t {
     uint32_t sender;
 };
 
-
+/* For information about this file, see the `MiniOS Programmer's Manual.` */
 uint32_t send(msg_t* msg, uint32_t comm_channel);
 uint32_t recv(msg_t* msg_dest, uint32_t comm_channel);
-uint32_t sleep(uint32_t ticks);
+// uint32_t sleep(uint32_t ticks);
 uint32_t exit();
 uint32_t spawn(uint32_t eip, uint32_t argc, char** argv);
 uint32_t spawn_bg(uint32_t eip, uint32_t argc, char** argv);
